5eb481d6adce580aca3c24f3f0a8b8307a6fccdf
elvinporter03-code <elvinporter03@gmail.com>
2025-10-15 19:47:05 +0200

    test

--- workout.py
+++ workout.py
@@ -1,3 +1,13 @@
+import pandas
+DataFrame=pandas.read_csv("excercises.csv")
 
+def get_excercises():
+    for index, row in DataFrame.iterrows():
+        print(f"Exercise: {row['Name']}")
+        print(f"Muscle: {row['Primary muscle']}")
+        print(f"Weight Multiplier: {row['Weight Multiplier']}")
+        print(f"Reps: {row['Reps']}, Weight: {row['Weight']}, 1RM: {row['1rm']}")
+        print("-" * 40)
 
-#hejsan
+if __name__ == "__main__":
+    get_excercises()
